3449e6520feeb8ad45dc75de6ef9981c6f7ee65f
juniorashraf02 <shuvo.istiak11@gmail.com>
2022-05-03 01:13:16 +0600

    pages with btns fixed

--- src/Components/SingleInventoryProduct/SingleInventoryProduct.js
+++ src/Components/SingleInventoryProduct/SingleInventoryProduct.js
@@ -29,24 +29,25 @@ const SingleInventoryProduct = () => {
     ];
     const randomNumber = Math.floor(Math.random() * textArray.length);
 
-    const handleDeliverBtn =(e)=> {
-       e.preventDefault();
+    const handleDeliverBtn = (e) => {
+        e.preventDefault();
         const itemQuantity = quantity;
-        const newQuantity=itemQuantity-1;
+        const newQuantity = itemQuantity - 1;
         console.log(newQuantity);
-        setProducts({...products, quantity:newQuantity});
+        setProducts({ ...products, quantity: newQuantity });
 
     }
 
-    
+
 
     return (
         <div>
             <div class="flex justify-center mt-20">
-                <div class="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
-                    <img class=" w-full h-96 md:h-auto object-cover md:w-60 rounded-t-lg md:rounded-none md:rounded-l-lg" src={img} alt={name} />
+                <div class="flex flex-col md:flex-row md:w-1/2 rounded-lg bg-white shadow-lg">
+                    <img class=" w-full h-60 md:h-auto object-cover md:w-84 rounded-t-lg md:rounded-none md:rounded-l-lg" src={img} alt={name} />
                     <div class="text-justify p-6 flex flex-col justify-start">
-                        <h5 class="text-gray-900 text-xl font-bold mb-2">Product Name:{name}</h5>
+                        <h5 class="text-gray-900 text-xl font-bold mb-2">
+                            Product Name:  {name}</h5>
                         <p class="text-gray-400 text-base mb-4">
                             {description}
                         </p>
@@ -61,8 +62,23 @@ const SingleInventoryProduct = () => {
                         </p>
                         <p class="text-gray-600 text-xs mt-3">Last updated {time} mins ago</p>
 
-                        <button onClick={handleDeliverBtn} type="button" class="mt-5 text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 dark:focus:ring-green-800 shadow-lg shadow-green-500/50 dark:shadow-lg dark:shadow-green-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Delivered</button>
-                        <button type="button" class=" inline-block px-6 py-2 border-2 border-gray-200 cursor-not-allowed text-gray-600 font-bold text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">{textArray[randomNumber]}</button>
+
+
+
+                        <div class="flex justify-center items-center my-2">
+                            <div class=" ">
+
+                                <input type="text" class=" form-control block w-full px-3 py-1.5 text-center font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" id="exampleText0" placeholder="Text input" />
+                            </div>
+                            <div class="">
+                                <button type="button" class="inline-block px-6 py-3 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 ml-2 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out">ADD</button>
+                            </div>
+
+                        </div>
+                        <button type="button" class="inline-block px-6 py-2.5 bg-gray-800 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-gray-900 hover:shadow-lg focus:bg-gray-900 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-900 active:shadow-lg transition duration-150 ease-in-out">Delivered</button>
+
+
+                        <button type="button" class="mt-1 inline-block px-6 py-2 border-2 border-gray-200 cursor-not-allowed text-gray-600 font-bold text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out">{textArray[randomNumber]}</button>
 
                     </div>
                 </div>
